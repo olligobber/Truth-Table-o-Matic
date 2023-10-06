b1844e251b66c92409fddb7fe0a45ca42aa6902e
olligobber <sirolligobbervii@yahoo.com.au>
2023-10-06 16:54:16 +1100

    Fixed various parts of IO

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,12 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
-import System.IO (hFlush, stdout)
+import System.IO (hFlush, stdout, stderr)
 import qualified Control.Monad.Writer as W
-import Control.Monad (foldM, when)
+import Control.Monad (foldM)
 import Data.String (fromString)
+import System.Exit (exitFailure)
+import Control.Applicative ((<|>))
 
 import TruthTable (TruthTable, addProp, addForm, addDef, empty)
 import Logging (Log, Level(Report, Warning, Error), printLog, err)
@@ -17,7 +19,9 @@ import Render (render, putRender)
 
 -- Simple prompt for text input
 prompt :: String -> IO Text
-prompt p = putStr p >> hFlush stdout >> TIO.getLine
+prompt p =
+	(putStr p *> hFlush stdout *> TIO.getLine) <|>
+	("" <$ putStrLn "")
 
 -- Prompt for a new columns to add, returning a truth table after all are added
 promptCols :: TruthTable Text -> IO (TruthTable Text)
@@ -30,24 +34,26 @@ promptCols table = do
 			Right [] -> error "Impossible output from splitOn"
 			Right [Prop x] -> do
 				let (newtable, l) = W.runWriter $ addProp table x
-				_ <- printLog l Report
+				_ <- printLog stdout l Report
 				promptCols newtable
 			Right [w] -> do
 				let (newtable, l) = W.runWriter $ addForm table w
-				_ <- printLog l Report
+				_ <- printLog stdout l Report
 				promptCols newtable
 			Right [Prop x, w] -> do
 				let (newtable, l) = W.runWriter $ addDef table x w
-				_ <- printLog l Report
+				_ <- printLog stdout l Report
 				promptCols newtable
 			Right [w, Prop x] -> do
 				let (newtable, l) = W.runWriter $ addDef table x w
-				_ <- printLog l Report
+				_ <- printLog stdout l Report
 				promptCols newtable
 			Right _ ->
-				putStrLn "Invalid format for new column" >> promptCols table
+				putStrLn "Invalid format for new column" *> promptCols table
 			-- Parsing error, print to user
-			Left e -> print e >> promptCols table
+			Left e -> do
+				_ <- printLog stdout (err $ fromString $ show e) Report
+				promptCols table
 
 -- Make a truth table from an indexed list of columns to add
 makeTable :: [(Integer, Text)] -> W.Writer Log (TruthTable Text)
@@ -88,6 +94,9 @@ main = do
 			let (table, l) =
 					W.runWriter $ makeTable $ zip [1..] $ fromString <$> args
 			-- Print out warnings and errors
-			logtype <- printLog l Warning
+			logtype <- printLog stderr l Warning
 			-- Print truth table if no errors occurred
-			when (logtype /= Just Error) $ putRender table
+			if logtype == Just Error then
+				exitFailure
+			else
+				putRender table
--- lib/Logging.hs
+++ lib/Logging.hs
@@ -10,8 +10,10 @@ module Logging
 	) where
 
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Data.Foldable (traverse_)
+import System.IO (Handle, hPutStrLn)
 
 -- A monoid for logging at various levels
 data Log = Log {
@@ -31,35 +33,39 @@ instance Monoid Log where
 
 -- Create a log with an error
 err :: Text -> Log
-err e = Log [e] [] []
+err e = Log (T.lines e) [] []
 
 -- Create a log with a warning
 warn :: Text -> Log
-warn w = Log [] [w] []
+warn w = Log [] (T.lines w) []
 
 -- Create a log with a report
 report :: Text -> Log
-report r = Log [] [] [r]
+report r = Log [] [] (T.lines r)
 
 -- Output indented
-putIndent :: Text -> IO ()
-putIndent x = TIO.putStrLn $ "\t" <> x
+putIndent :: Handle -> Text -> IO ()
+putIndent h x = TIO.hPutStrLn h $ "\t" <> x
 
 -- Print logs above a certain level, printing and returning the highest level
-printLog :: Log -> Level -> IO (Maybe Level)
-printLog (Log [] _ _) Error = return Nothing
-printLog (Log [] [] _) Warning = return Nothing
-printLog (Log [] [] []) _ = return Nothing
-printLog (Log e@(_:_) _ _) _= do
-	putStrLn ""
-	putStrLn "The following errors occurred:"
-	traverse_ putIndent e
-	putStrLn ""
-	return $ Just Error
-printLog (Log [] w@(_:_) _) _ = do
-	putStrLn ""
-	putStrLn "The following warnings have triggered:"
-	traverse_ putIndent w
-	putStrLn ""
-	return $ Just Warning
-printLog (Log [] [] r@(_:_)) _ = Just Report <$ traverse_ putIndent r
+printLog :: Handle -> Log -> Level -> IO (Maybe Level)
+printLog _ (Log [] _ _) Error = pure Nothing
+printLog _ (Log [] [] _) Warning = pure Nothing
+printLog _ (Log [] [] []) _ = pure Nothing
+printLog h (Log e@(_:_) _ _) _= do
+	hPutStrLn h ""
+	hPutStrLn h "The following errors occurred:"
+	traverse_ (putIndent h) e
+	hPutStrLn h ""
+	pure $ Just Error
+printLog h (Log [] w@(_:_) _) _ = do
+	hPutStrLn h ""
+	hPutStrLn h "The following warnings have triggered:"
+	traverse_ (putIndent h) w
+	hPutStrLn h ""
+	pure $ Just Warning
+printLog h (Log [] [] r@(_:_)) _ = do
+	hPutStrLn h ""
+	traverse_ (putIndent h) r
+	hPutStrLn h ""
+	pure $ Just Report
